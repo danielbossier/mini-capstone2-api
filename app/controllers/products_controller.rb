c280506542c8b5ef4b6c0a27da7209e984d7cc86
danielbossier <dbossier21@gmail.com>
2021-06-18 10:57:23 -0700

    added create in post

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+  def index
+    products = Product.all
+    render json: products.as_json
+  end
+
+  def show
+    products = Product.find(params[:id])
+    render json: products.as_json
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    product.save
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/products" => "products#index"
+
+  get "/products/:id" => "products#show"
+
+  get "/one_product" => "products#one_product"
+
+  post "/products" => "products#create"
 end
