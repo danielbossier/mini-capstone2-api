fd50d6a1b926256a93bb6d9981b7e8b0f801bfb5
danielbossier <dbossier21@gmail.com>
2021-06-30 09:51:49 -0700

    added authorization for user orders and create order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,20 @@
 class OrdersController < ApplicationController
   def create
     product = Product.find_by(id: params[:product_id])
+    calculated_subtotal = product.price * params[:quantity].to_i
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     order = Order.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
-      subtotal: product.price * params[:quantity],
-      tax: product.tax * params[:quantity],
-      total: product.total * params[:quantity],
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
-    if order.save
+    if current_user
+      order.save
       render json: order.as_json
     else
       render json: { error: order.errors.full_messages }
@@ -17,14 +22,16 @@ class OrdersController < ApplicationController
   end
 
   def show
-    if current_user
-      order = Order.find(params[:id])
-      render json: order
-    end
+    order = current_user.orders.find_by(id: params[:id])
+    render json: order
   end
 
   def index
-    order = Order.all
-    render json: order
+    if current_user
+      orders = current_user.orders
+      render json: orders
+    else
+      render json: [], status: :unauthorized
+    end
   end
 end
