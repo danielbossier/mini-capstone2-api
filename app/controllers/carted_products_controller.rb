afbfbeb01446ad25f287905a8f95123d08a2f7e1
danielbossier <dbossier21@gmail.com>
2021-07-07 10:36:11 -0700

    added current user to carted products controller

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,8 +1,11 @@
 class CartedProductsController < ApplicationController
+  before_action :authenticate_user
+
   def create
     carted_product = CartedProduct.new(
       quantity: params["quantity"],
       product_id: params["product_id"],
+      user_id: current_user.id,
     )
     if carted_product.save
       render json: carted_product.as_json
