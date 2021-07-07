4e678508ff2e3be66cfb3e8eec84ecd75acc669d
danielbossier <dbossier21@gmail.com>
2021-07-07 10:53:36 -0700

    added index to carted_product and carted status

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -6,12 +6,18 @@ class CartedProductsController < ApplicationController
       quantity: params["quantity"],
       product_id: params["product_id"],
       user_id: current_user.id,
+      status: "carted",
     )
     if carted_product.save
-      render json: carted_product.as_json
+      render json: carted_product
     else
       render json: { error: carted_product.errors.full_messages },
              status: :unprocessable_entity
     end
+  end
+
+  def index
+    carted_products = current_user.carted_products
+    render json: carted_products
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,4 +16,5 @@ Rails.application.routes.draw do
   post "/sessions" => "sessions#create"
 
   post "/carted_products" => "carted_products#create"
+  get "/carted_products" => "carted_products#index"
 end
